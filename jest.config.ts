96efb55b1a899e0bd187b92d6185a8cfbdb48db8
Tobias Wälde <t.waelde@pm.me>
2026-08-26 00:41:01 +0200

    test: enforce full coverage

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,10 @@
 import type { Config } from 'jest';
 const config: Config = {
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts'],
+  coverageThreshold: {
+    global: { branches: 100, functions: 100, lines: 100, statements: 100 },
+  },
   moduleFileExtensions: ['js', 'json', 'ts'],
   testRegex: '.*\\.spec\\.ts$',
   transform: { '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }] },
